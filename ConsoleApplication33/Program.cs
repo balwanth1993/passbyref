c287e685cd97b52225b4666d4505228c20195ed1
balwanth1993 <balwanth.r93@gmail.com>
2016-12-03 18:20:46 -0600

    Add project files.

---
+++ ConsoleApplication33/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication33
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter the value of a : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Value of 'a' before the function : {0}", a);
+            
+            Sum(ref a);
+
+           
+            Console.WriteLine("Value of 'a' after  the function : {0}", a);
+
+
+        }
+        public static void Sum(ref int x)
+        {
+
+            x = x + 100;
+            Console.WriteLine("Result is " + x);
+        }
+    }
+}
